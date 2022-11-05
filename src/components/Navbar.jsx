2a3aeb2c529e2e2586817c1106e679bd03a3c0d2
Tony5300 <tony_tolen@ziggo.nl>
2022-11-05 14:08:45 +0100

    feat: fixed the navbar bug and fixed the links to different pages.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,112 +8,71 @@ function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav>
-      <Link to="/">
-        <button>HOME</button>
-      </Link>
-      <Link to="/all-posts">
-        <button>POST</button>
-      </Link>
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div className="container">
+        <a className="navbar-brand" href="#page-top">
+          <img src="./img/navbar-logo.svg" alt="..." />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          Menu
+          <i className="fas fa-bars ms-1"></i>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+            <div className="nav-item">
+              <Link className="nav-link" to="/">
+                HOME
+              </Link>
+            </div>
+            <div className="nav-item">
+              <Link className="nav-link" to="/all-posts">
+                POST
+              </Link>
+            </div>
 
-      {isLoggedIn && (
-        <>
-          <Link to="/dashboard">
-            <button>DASHBOARD</button>
-          </Link>
-          <button onClick={logOutUser}>LOGOUT</button>
+            {isLoggedIn && (
+              <>
+                <div className="nav-item">
+                  <Link className="nav-link" to="/dashboard">
+                    Dashboard
+                  </Link>
+                </div>
+                <button onClick={logOutUser}>LOGOUT</button>
+                <div className="nav-item">
+                  <Link className="nav-link" to="/profile">
+                    Profile
+                  </Link>
+                </div>
+                <span>{user && user.username}</span>
+              </>
+            )}
 
-          {/* <Link to="/profile">
-            <button>Profile</button>
-            <img
-              src="https://picsum.photos/id/402/200/300"
-              style={{ width: 50, height: 50, borderRadius: 25 }}
-              alt="profile"
-            />
-          </Link> */}
-
-          <span>{user && user.username}</span>
-        </>
-      )}
-
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup">
-            {" "}
-            <button>SIGNUP</button>{" "}
-          </Link>
-          <Link to="/login">
-            {" "}
-            <button>LOGIN</button>{" "}
-          </Link>
-        </>
-      )}
+            {!isLoggedIn && (
+              <>
+                <div className="nav-item">
+                  <Link className="nav-link" to="/signup">
+                    Signup
+                  </Link>
+                </div>
+                <div className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </div>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
     </nav>
-
-    // <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-    //   <div className="container">
-    //     <a className="navbar-brand" href="#page-top">
-    //       <img src="./img/navbar-logo.svg" alt="..." />
-    //     </a>
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#navbarResponsive"
-    //       aria-controls="navbarResponsive"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       Menu
-    //       <i className="fas fa-bars ms-1"></i>
-    //     </button>
-    //     <div className="collapse navbar-collapse" id="navbarResponsive">
-    //       <ul className="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
-    //         <li className="nav-item">
-    //           <a className="nav-link" href="/login">
-    //             Home
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a className="nav-link" href="/all-posts">
-    //             Post
-    //           </a>
-    //         </li>
-    //         {isLoggedIn && (
-    //           <>
-    //             <li className="nav-item">
-    //               <a className="nav-link" href="/dashboard">
-    //                 Dashboard
-    //               </a>
-    //             </li>
-    //             <li className="nav-item">
-    //               <a className="nav-link" href="/logout" onClick={logOutUser}>
-    //                 Logout
-    //               </a>
-    //             </li>
-    //           </>
-    //         )}
-
-    //         {!isLoggedIn && (
-    //           <>
-    //             <li className="nav-item">
-    //               <a className="nav-link" href="/signup">
-    //                 Signup
-    //               </a>
-    //             </li>
-    //             <li className="nav-item">
-    //               <a className="nav-link" href="/login">
-    //                 Login
-    //               </a>
-    //             </li>
-    //           </>
-    //         )}
-
-    //         <span>{user && user.username}</span>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
   );
 }
 
